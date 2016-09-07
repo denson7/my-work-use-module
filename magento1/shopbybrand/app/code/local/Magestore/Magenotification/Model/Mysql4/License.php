91d43b97315cbedb8ba44a9e610f1aa87f3e634c
denson.liu <denson.liu@silksoftware.com>
2016-09-07 10:01:58 +0800

    移除shopbybrand

--- magento1/shopbybrand/app/code/local/Magestore/Magenotification/Model/Mysql4/License.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-class Magestore_Magenotification_Model_Mysql4_License extends Mage_Core_Model_Mysql4_Abstract
-{
-    protected function _construct(){
-        $this->_init('magenotification/license', 'license_id');
-    }
-}
