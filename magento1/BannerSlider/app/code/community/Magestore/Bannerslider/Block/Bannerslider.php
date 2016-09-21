c7ab0028d9095a865be43ea459276363d13f94ea
denson.liu <denson.liu@silksoftware.com>
2016-09-21 11:14:19 +0800

    add bannerslide

--- magento1/BannerSlider/app/code/community/Magestore/Bannerslider/Block/Bannerslider.php
+++ magento1/BannerSlider/app/code/community/Magestore/Bannerslider/Block/Bannerslider.php
@@ -2,18 +2,18 @@
 
 /**
  * Magestore
- * 
+ *
  * NOTICE OF LICENSE
- * 
+ *
  * This source file is subject to the Magestore.com license that is
  * available through the world-wide-web at this URL:
  * http://www.magestore.com/license-agreement.html
- * 
+ *
  * DISCLAIMER
- * 
+ *
  * Do not edit or add to this file if you wish to upgrade this extension to newer
  * version in the future.
- * 
+ *
  * @category 	Magestore
  * @package 	Magestore_Bannerslider
  * @copyright 	Copyright (c) 2012 Magestore (http://www.magestore.com/)
@@ -22,20 +22,20 @@
 
 /**
  * Bannerslider Block
- * 
+ *
  * @category 	Magestore
  * @package 	Magestore_Bannerslider
  * @author  	Magestore Developer
  */
 class Magestore_Bannerslider_Block_Bannerslider extends Mage_Core_Block_Template {
 
-    protected function _toHtml() {         
-                if(!Mage::getStoreConfig('bannerslider/general/enable')){	
+    protected function _toHtml() {
+                if(!Mage::getStoreConfig('bannerslider/general/enable')){
                     return '';
                 }
-                //die('11111111');
+              //die(11);
         $collection = null;
-        $banners = array();        
+        $banners = array();
         $collection = Mage::getModel('bannerslider/bannerslider')->getCollection()
                 ->addFieldToFilter('status', 0)
                 ->addFieldToFilter('position', array(
@@ -52,6 +52,6 @@ class Magestore_Bannerslider_Block_Bannerslider extends Mage_Core_Block_Template
     public function getIsHomePage() {
         return $this->getUrl('') == $this->getUrl('*/*/*', array('_current' => true, '_use_rewrite' => true));
     }
-    
+
 
 }
