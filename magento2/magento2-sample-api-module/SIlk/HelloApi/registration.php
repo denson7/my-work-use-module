a9361ec318ea910d407dda99faddce09f4c953fd
denson.liu <denson.liu@silksoftware.com>
2016-09-23 11:17:40 +0800

    add magento2 simple api sample module

---
+++ magento2/magento2-sample-api-module/SIlk/HelloApi/registration.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: denson
+ * Date: 16-8-19
+ * Time: 上午9:42
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Silk_HelloApi',
+    __DIR__
+);
